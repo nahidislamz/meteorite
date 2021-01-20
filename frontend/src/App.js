49b997029aa866493a19955136489a6eab8b703e
Nahid <nahidislamz@outlook.com>
2021-01-20 22:48:49 +0600

    blog post and detailView added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Layout from './containers/Layout';
+import Blog from './components/Blog';
+import BlogDetail from './components/BlogDetail';
+import Category from './components/Category';
+
+const App = () => (
+    <Router>
+        <Layout>
+            <Switch>
+                <Route exact path='/' component={Blog} />
+                <Route exact path='/category/:id' component={Category} />
+                <Route exact path='/blog/:id' component={BlogDetail} />
+            </Switch>
+        </Layout>
+    </Router>
+);
 
 export default App;
